f3a797b9af84b86314aba02f1564946c43b825ea
Mitka Panarin <mitka_j@yahoo.com>
2023-05-19 02:15:26 +0200

    edit user modal

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useUpdateUserMutation } from '../store/API/userApi';
-import { updateUserStateData } from '../store/Slices/userSlice';
-import { toast } from 'react-toastify';
-import UserInformations from './UserInformations';
+import React, {useState} from 'react';
+import { useSelector } from 'react-redux';
+import { useGetUserQuery } from '../store/API/userApi';
+import EditUserModal from '../components/EditUserModal';
 
 const User = () => {
+  const state = useSelector(z => z)
+  console.log(state.User.username)
+
   const userData = useSelector((state) => state.User);
   console.log(userData);
 
@@ -13,89 +14,37 @@ const User = () => {
   console.log(userOwner);
 
   const [data, setData] = useState(userData);
+  const { oneUserData } = useGetUserQuery({ userID: userOwner });
 
-  const [updateUser] = useUpdateUserMutation();
-
-  const dispatch = useDispatch();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await updateUser(data)
-      dispatch(updateUserStateData(data))
-      toast.success('User updated successfully');
-    } catch (err) {
-      console.log('cannot update user', err);
-      toast.error("Couldn't update, please try again");
-    }
-  };
-
-  const onChange = (e) => {
-    setData({
-      ...data,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const userInformations = oneUserData?.User?.map(user => (
+    <tr key={user._id}>
+      <td className="border px-4 py-2">{User.username}</td>
+      <td className="border px-4 py-2">{User.email}</td>
+    </tr>
+  ))
 
   return (
-<>
-<UserInformations/>
-<form onSubmit={handleSubmit}>
-      <div className="mb-6">
-        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Enter your new username
-        </label>
-        <input
-          onChange={onChange}
-          value={data.username}
-          type="text"
-          name="username"
-          id="username"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="UserName"
-          required
-        />
+ <>
+    <div>
+      <h1 className='text-center text-4xl'>User informations</h1><br /><br />
+      <div className="mx-auto">
+        <table className="table-auto mx-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Username</th>
+              <th className="px-4 py-2">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userInformations}
+          </tbody>
+        </table>
+        <br />
       </div>
-      <div className="mb-6">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Enter your new Email address
-        </label>
-        <input
-          onChange={onChange}
-          value={data.email}
-          type="email"
-          name="email"
-          id="email"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="aaa@company.com"
-          required
-        />
-      </div>
-      <div className="mb-6">
-        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Enter your new Password
-        </label>
-        <input
-          onChange={onChange}
-          value={data.password}
-          name="password"
-          type="password"
-          id="password"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="•••••••••"
-          required
-        />
-      </div>
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Submit
-      </button>
-      <div className="text-2xl text-center"></div>
-    </form>
-</>
-  );
+    </div>
+    <EditUserModal/>
+ </>
+  )
 };
 
 export default User;
